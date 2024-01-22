eb1af5e413436bc5132179f4acb867f2ac40197a
Leonildo Camilo <leonildo_camilo@hotmail.com>
2024-01-22 14:08:18 -0300

    update

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -6,6 +6,9 @@ const TaskController = require('../controllers/TaskController')
 router.get('/add', TaskController.createTask)
 router.post('/add', TaskController.createTaskSave)
 router.post('/remove', TaskController.removeTask)
+router.get('/edit/:id', TaskController.updateTask)
+router.post('/edit', TaskController.updateTaskPosts)
+router.post('/updatestatus', TaskController.toggleTaskStatus)
 router.get('/', TaskController.showTasks)
 
 module.exports = router
